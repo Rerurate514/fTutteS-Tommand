ecd8c73e07dcecf0007665182f64426b631d7634
Rerurate514 <rerurateyuto@gmail.com>
2025-05-17 22:33:19 +0900

    versionUp 0.2.0

--- src/tommand/index.ts
+++ src/tommand/index.ts
@@ -8,7 +8,7 @@ const program = new Command();
 program
     .name("tommand")
     .description("setup ftuttes project")
-    .version("0.1.0");
+    .version("0.2.0");
 
 program.addCommand(createFtuttesCommand);
 
