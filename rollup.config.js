3d1043e84c37d63aac75e0d5ab465961cfd29ffb
Rerurate514 <rerurateyuto@gmail.com>
2025-05-17 20:06:19 +0900

    rollupコンフィグファイルの追加

---
+++ rollup.config.js
@@ -0,0 +1,34 @@
+import typescript from '@rollup/plugin-typescript';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
+
+const nodeExternals = [
+    'commander',
+    'events',
+    'fs',
+    'path',
+    'util',
+];
+
+const cliConfig = {
+    input: 'dist/tommand/index.js',
+    output: {
+        file: 'dist/cli.mjs',
+        format: 'es',
+        sourcemap: true
+    },
+    external: nodeExternals,
+    plugins: [
+        typescript({
+            tsconfig: './tsconfig.json'
+        }),
+        nodeResolve({
+            preferBuiltins: true,
+        }),
+        commonjs(),
+        terser()
+    ]
+};
+
+export default [cliConfig];
